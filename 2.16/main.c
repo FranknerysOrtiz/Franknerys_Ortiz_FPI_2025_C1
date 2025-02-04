564ada1646db5735597ce62ed15ad17c09466644
FranknerysOrtiz <franknerysortizsantana@gmail.com>
2025-02-03 20:35:56 -0400

    Add files via upload

---
+++ 2.16/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    int CLA, CAT, ANT, RES;
+    printf("\nIngrese la clave, categoría y antiguedad del trabajador:");
+    scanf("%d %d %d", &CLA, &CAT, &ANT);
+    switch(CAT)
+    {
+    case 3:
+        case 4: if (ANT >= 5)
+        RES = 1;
+        else
+            RES = 0;
+        break;
+        default: RES = 0;
+        break;
+    }
+    if (RES)
+        printf("\nEl trabajdor con calve %d reúne las condiciones para el puesto", CLA);
+    else
+     printf("\nEl trabajdor con calve %d  no reúne las condiciones para el puesto", CLA);
+
+
+
+
+}
